d1df24bf6bf0d007a8c521bba22c1ba9984c46c8
andvla <vladimir.andronov@gmail.com>
2023-03-20 13:35:58 +0200

    Revert "Upgraded lcobucci/jwt package and related changes by the upgrade"

--- src/JwtValidateServiceProvider.php
+++ src/JwtValidateServiceProvider.php
@@ -5,9 +5,6 @@ namespace Jobilla\JwtValidate;
 use Illuminate\Auth\RequestGuard;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
-use Lcobucci\JWT\Configuration;
-use Lcobucci\JWT\Signer\Key\InMemory;
-use Lcobucci\JWT\Signer\Rsa\Sha256;
 
 class JwtValidateServiceProvider extends ServiceProvider
 {
@@ -35,7 +32,7 @@ class JwtValidateServiceProvider extends ServiceProvider
     {
         return new RequestGuard(function ($request) use ($config) {
             return (new TokenValidationGuard(
-                Configuration::forSymmetricSigner(new Sha256(), InMemory::file($config['public_key_path'])),
+                $config['public_key_path'],
                 $config['hydrator']
             ))->user($request);
         }, $this->app['request']);
